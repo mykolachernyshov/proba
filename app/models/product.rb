142cfcc0429c7a0a0b30c2ffd288f279440bb449
mykolachernyshov <mykolaror@gmail.com>
2015-01-12 15:47:33 +0200

    validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ActiveRecord::Base
+	validates :title, :description, :image_url, :presence => 
+			{ :message => "не повинно бути порожнім" }
+ 	validates :title, uniqueness: {message: "вже викоритовується"}
+	validates :image_url, allow_blank: true, format: {
+				with: %r{\.(gif|jpg|png)\Z}i,
+				message: 'URL должен указывать на изображение формата GIF, JPG или PNG.'
+				}
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 end
